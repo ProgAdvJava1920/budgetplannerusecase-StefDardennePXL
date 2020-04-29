d8877c3329d1150d7e2f21924106b78c98efed87
StefDardenne <Stef.Dardenne@student.pxl.be>
2020-04-29 11:04:20 +0200

    Edited dialect property in persistence.xml, implemented Budgetplanner main to insert into database

--- src/main/java/be/pxl/student/BudgetPlanner.java
+++ src/main/java/be/pxl/student/BudgetPlanner.java
@@ -1,18 +1,59 @@
 package be.pxl.student;
 
+import be.pxl.student.entity.Account;
+import be.pxl.student.entity.DAO;
+import be.pxl.student.entity.Payment;
+import be.pxl.student.entity.exception.AccountException;
+import be.pxl.student.entity.exception.PaymentException;
+import be.pxl.student.entity.jpa.AccountJPA;
+import be.pxl.student.entity.jpa.PaymentJPA;
 import be.pxl.student.util.BudgetPlannerException;
 import be.pxl.student.util.BudgetPlannerImporter;
+import be.pxl.student.util.BudgetPlannerMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class BudgetPlanner {
+    private static Logger logger = LogManager.getLogger(BudgetPlanner.class);
     public static void main(String[] args) {
         Path csv = Paths.get("src/main/resources/account_payments.csv");
         try {
-            BudgetPlannerImporter.readCsvFile(csv);
-        } catch (BudgetPlannerException e) {
+            logger.info("Starting CSV import...");
+            List<String> csvLines = BudgetPlannerImporter.readCsvFile(csv);
+            logger.info("CSV import done!");
+
+            logger.info("Starting Account mapping...");
+            List<Account> accountList = new BudgetPlannerMapper().mapAccounts(csvLines);
+            logger.info("Account mapping done!");
+
+            logger.info("Inserting Accounts into DB...");
+            insertIntoDatabase(accountList);
+            logger.info("Inserting Accounts into DB done!");
+
+        } catch (BudgetPlannerException | AccountException | PaymentException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void insertIntoDatabase(List<Account> accountList) throws AccountException, PaymentException {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("budgetplanner");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        DAO<Account, AccountException> accountDAO = new AccountJPA(entityManager);
+        DAO<Payment, PaymentException> paymentDAO = new PaymentJPA(entityManager);
+
+        for (Account account : accountList) {
+            accountDAO.create(account);
+            for (Payment payment : account.getPayments()) {
+                paymentDAO.create(payment);
+            }
         }
     }
 }
